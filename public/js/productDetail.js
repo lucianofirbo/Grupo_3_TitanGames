8789e496a65ac87b6a6b65a015bc31b98c502f1d
EveNavarro <evenavarro.dn@gmail.com>
2021-08-01 16:36:25 -0300

    slider terminado en detalle del producto

--- public/js/productDetail.js
+++ public/js/productDetail.js
@@ -1,31 +1,27 @@
-window.addEventListener('load', function(){
-    new Glider(document.querySelector('.slider-list'), {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: '.slider-ind',
-        arrows: {
-            prev: '.slider-prev',
-            next: '.slider-next'
-        },
-        responsive: [
-            {
-              //>= 775px
-              breakpoint: 450,
-              settings: {
-                // Set to `auto` and provide item width to adjust to viewport
-                slidesToShow: '2',
-                slidesToScroll: '2',
-/*                 itemWidth: 150,
-                duration: 0.25 */
-              }
-            },{
-              //1024px
-              breakpoint: 769,
-              settings: {
-                slidesToShow: 4,
-                slidesToScroll: 4,
-              }
-            }
-          ]
-    });
-});
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
