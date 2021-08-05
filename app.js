b6e3dd5d50daa88c6815e860cade07666d8ef921
PabloBerrettoni <pablo_berrettoni@hotmail.com>
2021-08-05 19:37:35 -0300

    mas fixes de ultimo momento

--- app.js
+++ app.js
@@ -13,14 +13,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'))
 });
 
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/login.html'))
-});
-
-app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/register.html'))
-});
-
 app.get('/productDetail', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/productDetail.html'))
 });
