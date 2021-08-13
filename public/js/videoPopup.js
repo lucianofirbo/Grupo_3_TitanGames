6b8dda2097f4e4233bbd0d5e75b17a0a80851132
EveNavarro <evenavarro.dn@gmail.com>
2021-08-12 23:38:05 -0300

    borré el archivo videopopup.js

--- public/js/videoPopup.js
+++
@@ -1,8 +0,0 @@
-
-function toggle(){
-    var trailer = document.querySelector(".trailer")
-    var iframe = document.querySelector("iframe")
-    trailer.classList.toggle("active");
-    iframe.pause();
-    iframe.currentTime = 0;
-}
