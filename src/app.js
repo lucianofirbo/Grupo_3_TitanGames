a28a319f7279548865e67f7d3d509f16e898859e
EveNavarro <evenavarro.dn@gmail.com>
2021-08-15 19:37:27 -0300

    modificacion

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.listen(port, () => {
 
 /* Setting up static folder */
 
-app.use(express.static('../public'));
+app.use(express.static('./public'));
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
