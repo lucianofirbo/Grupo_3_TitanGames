8d88c2c18d061284d9387be20580c0e030b04d08
EveNavarro <evenavarro.dn@gmail.com>
2021-09-07 00:28:31 -0300

    agregué la ruta del listado de productos

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/productController')
 
+
+/* Ruta con el listado de todos los productos */
+router.get('/', controller.index);
 /* Ruta para el detalle del producto */
 router.get('/detail/:id', controller.detail);
 /* Ruta para el carrito de compras */
