9ea1fde508f9420f0cd08ed2f58e7819b34d89f6
PabloBerrettoni <pablo_berrettoni@hotmail.com>
2021-09-10 16:45:28 -0300

    cambie /addProducts por /products

---
+++ src/middlewares/express-middleware.js
@@ -0,0 +1,17 @@
+/* Validations */ 
+const { body } = require('express-validator')
+const formValidator = [
+    body('product').notEmpty().withMessage('Debes introducir el nombre del producto'),
+    body('precio').notEmpty().withMessage('Debes introducir el precio del producto'),
+    body('genero').notEmpty().withMessage('Debes introducir la categoria del producto'),
+    body('subGenero').notEmpty().withMessage('Debes introducir la sub-categoria del producto'),
+    body('descripcion').notEmpty().withMessage('Debes introducir una descripcion del producto'),
+    body('minimumVideo').notEmpty().withMessage('Debes introducir requisito del producto'),
+    body('minimumProcessor').notEmpty().withMessage('Debes introducir requisito del producto'),
+    body('minimumRam').notEmpty().withMessage('Debes introducir requisito del producto'),
+    body('recommendedVideo').notEmpty().withMessage('Debes introducir requisito del producto'),
+    body('recommendedProcessor').notEmpty().withMessage('Debes introducir requisito del producto'),
+    body('recommendedRam').notEmpty().withMessage('Debes introducir requisito del producto'),
+]
+
+module.exports = formValidator;
--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const validator = require('../middlewares/express-middleware');
 const controller = require('../controllers/adminController');
 const fileUpload = require('../middlewares/multerMiddleware');
 
-/* Rutas para añadir, editar stock o eliminar un producto */
 
 /* Main Admin */ 
 router.get('/', controller.adminMain)
 
 /* Add Product */
-router.get('/addProduct', controller.addRender);
+router.get('/products', controller.addRender);
 router.post('/addProduct', fileUpload.fields([
     { name: 'imagenProducto', maxCount: 1 },
     { name: 'imagenProducto2', maxCount: 1 },
     { name: 'imagenProducto3', maxCount: 1 },
     { name: 'imagenProducto4', maxCount: 1 },
-    { name: 'imagenProducto5', maxCount: 1 }]), controller.addProduct);
+    { name: 'imagenProducto5', maxCount: 1 }]), validator, controller.addProduct);
 
-/* Edit Producto*/
+/* Edit Product */
 router.get('/editProduct/:id', controller.editRender);
 router.put('/editProduct/:id', fileUpload.fields([
     { name: 'imagenProducto', maxCount: 1 },
