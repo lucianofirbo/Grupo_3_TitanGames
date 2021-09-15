6b6e6da58de1193e4519e1516a8a30cdcda6aef6
EveNavarro <evenavarro.dn@gmail.com>
2021-09-15 12:38:54 -0300

    implemento las rutas del login

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,13 +1,16 @@
 const express = require ('express');
 const router = express.Router();
-const { processRegister, indexProfile, renderRegister } = require('../controllers/usersController');
-const uploadAvatar = require ('../middlewares/uploadAvatar')
-
+const { processRegister, indexProfile, renderRegister, renderLogin, processLogin } = require('../controllers/usersController');
+const uploadAvatar = require ('../middlewares/uploadAvatar');
+const loginValidator = require('../validations/loginValidator');
 
 /* Ruta del perfil */
 router.get('/profile', indexProfile);
 /* Ruta del registro */
 router.get('/register', renderRegister);
 //router.post('/register',uploadAvatar.single('avatar'), registerValidator, processRegister)
+/* Ruta para login */
+router.get('/login', renderLogin);
+router.post('/login', loginValidator ,processLogin);
 
 module.exports = router;
