e85ad294ee952a757862b42a656a07847ba41471
EveNavarro <evenavarro.dn@gmail.com>
2021-09-15 16:14:32 -0300

    cookies y session en el metodo processLogin

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -63,18 +63,35 @@ renderRegister: (req, res) => {
     },
 
     renderLogin: (req, res) => {
-        res.render('users/login');
+        res.render('users/login', {
+            session: req.session
+        });
     },
     processLogin: (req, res) => {
         let errors = validationResult(req)
 
         if(errors.isEmpty()) {
-            /* res.render('/users/login', {
-                dataBase: getProducts
-            }) */
+            let user = getUsers.find(user => user.email === req.body.email)
+
+            req.session.user = {
+                id: user.id,
+                userName: user.userName,
+                email: user.email,
+                avatar: user.avatar,
+                rol: user.rol
+            }
+
+            if (req.body.recordar) {
+                res.cookie("userTitanGames", req.session.user, {expires: new Date(Date.now() + 900000), httpOnly: true});
+            }
+
+            res.locals.user = req.session.user
+            res.redirect('/users/index');
+
         } else {
             res.render('users/login', {
                 errors: errors.mapped(),
+                session: req.session
             })
         }
     }
