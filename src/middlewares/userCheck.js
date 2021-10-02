117579bcceb710d9806fa09232b71c2872ff4a65
EveNavarro <evenavarro.dn@gmail.com>
2021-10-02 15:56:35 -0300

    modificacion para que redireccione al login

--- src/middlewares/userCheck.js
+++ src/middlewares/userCheck.js
@@ -2,6 +2,6 @@ module.exports = (req, res, next) => {
     if(req.session.userLogged){
       next()
     }else{
-        res.send('no pa tenes que loguearte')
+        res.redirect('/user/login')
     }
 }
