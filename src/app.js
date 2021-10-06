3baf5b0b1c96bdac6586ff33b78975c79425a8a5
EveNavarro <evenavarro.dn@gmail.com>
2021-10-05 22:14:31 -0300

    error404

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const port = 3000;
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const localsCheck = require('./middlewares/localsCheck');
+const createError = require('http-errors');
 
 /* Setting up the server */ 
 
@@ -42,4 +43,20 @@ app.use('/admin', adminRouter)
 
 const userRouter = require ('./routes/userRouter');
 app.use('/user', userRouter)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error 
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 
