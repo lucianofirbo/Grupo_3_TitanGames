01025564abce84fe1d85758263d9811c80dc567b
PabloBerrettoni <pablo_berrettoni@hotmail.com>
2021-10-23 04:14:13 -0300

    pusheo json

---
+++ public/js/profile.js
@@ -0,0 +1,47 @@
+window.onload = () => {
+
+    document.addEventListener('click', (event) => {
+        let objeto = event.target;
+        let idElemento = objeto.getAttribute('id');
+        if (idElemento === 'hamburguer-icon'){
+            document.getElementById('hamburguer').style.display = 'flex'
+        } else if (idElemento=== 'active2') {       
+            showHideCategories('active2', 'active', 'active3')
+            showHide('top-sales', 'newest', 'discover-games')
+        } else if (idElemento=== 'active3') {
+            showHideCategories('active3', 'active2', 'active')
+            showHide('newest', 'top-sales', 'discover-games')
+        } else if (idElemento=== 'active') {            
+            showHideCategories('active', 'active2', 'active3')
+            showHide('discover-games', 'newest', 'top-sales')
+        } else if (idElemento === 'hamburguer-account' || idElemento === 'redirect-login') {
+            document.getElementById('login-card').style.display = 'flex'            
+            document.getElementById('register-card').style.display = 'none'
+        } else if (idElemento === 'user-icon') {
+            document.getElementById('login-card').style.display = 'flex'
+        } else if (idElemento === 'store-hamburguer') {
+            document.getElementById('store-hamburguer-dropdown').style.display = 'flex'
+        } else if (idElemento === 'notificacion-hamburguer') {
+            document.getElementById('notificacion-hamburguer-dropdown').style.display = 'flex'
+        } else if (idElemento === 'close') {
+            document.getElementById('hamburguer').style.display = 'none'
+        } else if (idElemento === 'showHideBar') {
+            document.getElementById('userData').style.display = 'none'
+        }
+    })
+
+}
+
+function showHideCategories (idShow, idHide, idHide2) {
+    document.getElementById(idShow).style.backgroundColor = '#807c7c8a'
+    document.getElementById(idHide).style.backgroundColor = 'rgba(0, 0, 0, 0)'
+    document.getElementById(idHide2).style.backgroundColor = 'rgba(0, 0, 0, 0)'
+}
+
+function showHide (idShow, idHide, idHide2) {
+    document.getElementById(idShow).style.display = 'flex'
+    document.getElementById(idHide).style.display = 'none'
+    document.getElementById(idHide2).style.display = 'none'
+}
+
+/* document.getElementById('showHideBar') */
