c322ca11cdf1df1632e007a63dbb7fb337411ac2
EveNavarro <evenavarro.dn@gmail.com>
2021-11-25 17:55:49 -0300

    rutas para las apis

--- src/app.js
+++ src/app.js
@@ -44,6 +44,13 @@ app.use('/admin', adminRouter)
 const userRouter = require ('./routes/userRouter');
 app.use('/user', userRouter)
 
+/* Routes for Apis */
+const apiProductsRouter = require('./routes/api/productRouter');
+app.use('/api', apiProductsRouter);
+
+const apiCategoriesRouter = require('./routes/api/categoriesRouter');
+app.use('/api/categories', apiCategoriesRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
