d7d8585f59ed2595811cda00b0bcdb6b7177df01
EveNavarro <evenavarro.dn@gmail.com>
2021-11-25 17:57:04 -0300

    rutas de las apis

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -5,11 +5,13 @@ const fileUpload = require('../middlewares/multerMiddleware');
 const adminCheck = require('../middlewares/adminCheck');
 const productValidator = require('../validations/productCreateValidator');
 const productEditValidator = require('../validations/editProductValidator');
-
-/* Rutas para añadir, editar stock o eliminar un producto */
+const {categories, categoryCreateForm, categoryStore, categoryEdit, categoryUpdate, categoryDestroy} = require('../controllers/adminCategoriesController')
+const {subcategories, subcategoryCreateForm, subcategoryStore, subcategoryEdit, subcategoryUpdate, subcategoryDestroy} = require('../controllers/adminSubcategoriesController')
 
 /* Main Admin */ 
-router.get('/adminMain/:id', adminCheck, controller.adminMain)
+router.get('/adminMain/:id', adminCheck, controller.adminMain);
+
+/** ** **  PRODUCTS CRUD ** ** **/
 
 /* Add Product */
 router.get('/products', adminCheck, controller.addRender);
@@ -19,8 +21,7 @@ router.post('/products', fileUpload.fields([
     { name: 'imagenProducto3', maxCount: 1 },
     { name: 'imagenProducto4', maxCount: 1 },
     { name: 'imagenProducto5', maxCount: 1 }]), productValidator,controller.addProduct);
-
-/* Edit Producto*/
+/* Edit Product*/
 router.get('/editProduct/:id', adminCheck, controller.editRender);
 router.put('/editProduct/:id', fileUpload.fields([
     { name: 'imagenProducto', maxCount: 1 },
@@ -28,8 +29,28 @@ router.put('/editProduct/:id', fileUpload.fields([
     { name: 'imagenProducto3', maxCount: 1 },
     { name: 'imagenProducto4', maxCount: 1 },
     { name: 'imagenProducto5', maxCount: 1 }]), productEditValidator,controller.editProduct);
-
 /* Delete Product */
 router.delete('/deleteProduct/:id', controller.deleteProduct);
+
+/** ** **  CATEGORIES CRUD ** ** **/
+
+/* All categories */
+router.get('/categories', adminCheck, categories);
+/* Add categories */
+router.get('/categories/create', adminCheck, categoryCreateForm);
+router.post('/categories/create', adminCheck, categoryStore);
+/* Edit category */
+router.get('/categories/edit/:id', adminCheck, categoryEdit);
+router.put('/categories/edit/:id', adminCheck, categoryUpdate);
+/* Delete category */
+router.delete('/categories/delete/:id', categoryDestroy);
+
+/** ** **  SUBCATEGORIES CRUD ** ** **/
+
+/* All subcategories */
+router.get('/subcategories', adminCheck, subcategories);
+/* Add subcategories */
+router.get('/subcategories/create', adminCheck, subcategoryCreateForm);
+router.post('/subcategories/create', adminCheck, subcategoryStore);
 
 module.exports = router;
