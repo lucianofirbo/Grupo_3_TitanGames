0e6ddb5ba2db1ca662bf8eb6c564f9445598461f
EveNavarro <evenavarro.dn@gmail.com>
2021-11-29 19:42:00 -0300

    script actualizado

--- src/db/titan_games.sql
+++ src/db/titan_games.sql
@@ -33,7 +33,7 @@ CREATE TABLE `addresses` (
   PRIMARY KEY (`id`),
   KEY `userId` (`userId`),
   CONSTRAINT `addresses_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +42,7 @@ CREATE TABLE `addresses` (
 
 LOCK TABLES `addresses` WRITE;
 /*!40000 ALTER TABLE `addresses` DISABLE KEYS */;
-INSERT INTO `addresses` VALUES (2,'asd123','','',0,1212,12),(10,'asd123','','',0,2222,10),(12,'Richieri 613','','',0,1661,16);
+INSERT INTO `addresses` VALUES (2,'asd123','','',0,1212,12),(10,'asd123','','',0,2222,10),(12,'Richieri 613','','',0,1661,16),(13,'','','',0,0,17);
 /*!40000 ALTER TABLE `addresses` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -86,7 +86,7 @@ CREATE TABLE `categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -95,7 +95,7 @@ CREATE TABLE `categories` (
 
 LOCK TABLES `categories` WRITE;
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` VALUES (1,'Aventura'),(2,'Shooter');
+INSERT INTO `categories` VALUES (1,'Aventura'),(2,'Acción'),(11,'Rol'),(12,'Simulación'),(13,'Estrategia'),(14,'Deportes');
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -125,9 +125,9 @@ CREATE TABLE `products` (
   PRIMARY KEY (`id`),
   KEY `FK_ef58d02f-4de8-4f36-bdcd-d7fc35ed0d2a` (`categoryId`),
   KEY `subCategoryId` (`subCategoryId`),
-  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`subCategoryId`) REFERENCES `subcategories` (`id`),
-  CONSTRAINT `products_ibfk_2` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+  CONSTRAINT `products_ibfk_2` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`id`),
+  CONSTRAINT `products_ibfk_3` FOREIGN KEY (`subCategoryId`) REFERENCES `subcategories` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -136,7 +136,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (1,'The Witcher 3',600,'Mientras la guerra se extiende por los Reinos del Norte, aceptarás el contrato de tu vida: encontrar a la niña de la profecía, un arma viviente que puede alterar el mundo tal y como lo conocemos. ',10,'1050ti','I5 7400','8gb','1070ti','i7 8100','16gb',NULL,1,3,6),(2,'Battlefield 4',1200,'Esta es la mejor experiencia de Battlefield 4. Vive el mayor conflicto de la humanidad con un completo arsenal de armas, vehículos y dispositivos, y todos los contenidos de personalización de los dos primeros años ',25,'Nvidia GeForce 8800 GT','(Intel): Core 2 Duo 2.4 GHz','4 GB','NVIDIA GeForce GTX 660','(Intel): Quad-core CPU','8 GB de RAM',NULL,2,2,6),(11,'Ni no Kuni™ II: Revenant Kingdom',649,'Únete al viaje que llevará al joven rey Evan en busca de un nuevo reino, además de, con la ayuda de nuevos amigos, unir el mundo y salvar a sus gentes de un mal espantoso. ',NULL,'NVIDIA GeForce GTX 750 Ti','Intel Core i5-4460','4 GB de RAM','NVIDIA GeForce GTX 970','Intel Core i7-3770','8 GB de RAM','https://youtu.be/dQw4w9WgXcQ',1,1,6),(12,'Hearts of Iron IV',439,'¡Tenemos la victoria al alcance de la mano! Tu capacidad para liderar tu nación es tu arma principal. En el juego de estrategia Hearts of Iron IV podrás ponerte el mando de cualquier nación de la II Guerra Mundial, el conflicto más fascinante de la historia mundial. ',NULL,'ATI Radeon HD 5850','Intel Core 2 Quad Q9400 @ 2.66 GHz','4 GB de RAM','ATI Radeon HD 6950','Intel Core i5 750 @ 2.66 GHz','4 GB de RAM','https://youtu.be/dQw4w9WgXcQ',2,3,1),(13,'Europa Universalis IV',439,'Cuatro siglos de historia cobran vida en el juego de gran estrategia de Paradox. Dirige una nación desde el Renacimiento hasta la Revolución en una compleja simulación del comienzo del mundo moderno. Domina el arte de la guerra, la diplomacia y el comercio para cambiar la historia de tu nación.',NULL,'Nvidia® GeForce™ GTX 460','Intel® Core™ i3-2105','1 GB RAM','Nvidia® GeForce™ GTX 560 Ti','Intel® Core™ i3 3240','8 GB RAM','https://youtu.be/dQw4w9WgXcQ',2,3,1),(15,'The Elder Scrolls IV: Oblivion',240,'The Elder Scrolls IV: Oblivion® Edición Juego del Año presenta uno de los mejores juegos de rol de todos los tiempos como nunca se había visto hasta ahora. Adéntrate en el mundo más detallado y emocionante jamás creado. ',NULL,'Tarjeta gráfica con 128 MB de VRAM','Intel Pentium 4 a 2 GHz','512 MB de RAM','Tarjeta gráfica con 128 MB de VRAM','Intel Pentium 4 a 2 GHz','512 MB de RAM','https://youtu.be/dQw4w9WgXcQ',1,1,0),(17,'Happy Game',272,'Un niño se duerme y queda sumido en una pesadilla horrible. ¿Conseguirás devolverle la felicidad?',NULL,'DirectX 11 compatible GPU','2 GHz Intel i5','4 GB de RAM','DirectX 11 compatible GPU','2 GHz Intel i5','4 GB de RAM','https://youtu.be/dQw4w9WgXcQ',1,1,3),(18,'The Forest',89,'As the lone survivor of a passenger jet crash, you find yourself in a mysterious forest battling to stay alive against a society of cannibalistic mutants. Build, explore, survive in this terrifying first person survival horror simulator',60,'NVIDIA GeForce 8800GT','Intel Dual-Core 2.4 GHz','4 GB de RAM','NVIDIA GeForce GTX 560','Quad Core Processor','4 GB de RAM','https://youtu.be/dQw4w9WgXcQ',1,1,8),(19,'Age of Empires IV',2999,'Uno de los juegos de estrategia en tiempo real más queridos vuelve a demostrar todo su esplendor con Age of Empires IV, donde serás la estrella de las épicas batallas históricas que moldearon el mundo que hoy conocemos',NULL,'AMD Radeon RX Vega 11','Intel Core i5-6300U','8 GB de RAM','Nvidia GeForce 970 GPU','3.6 GHz 6-core (Intel i5)','16 GB de RAM','https://youtu.be/dQw4w9WgXcQ',1,1,1),(20,'Dead By Daylight',334,'Dead by Daylight es un juego de horror de multijugador (4 contra 1) en el que un jugador representa el rol del asesino despiadado y los 4 restantes juegan como supervivientes que intentan escapar de él para evitar ser capturados y asesinados',NULL,'GeForce GTX 460 1GB ','Intel Core i3-4170','8 GB de RAM','AMD HD 8800','AMD FX-8300','16 GB de RAM','https://youtu.be/dQw4w9WgXcQ',1,1,2),(22,'Naraka: Bladepoint',1499,'NARAKA: BLADEPOINT es una experiencia mítica de combate y acción para hasta 60 jugadores con enfrentamientos cuerpo a cuerpo basados en las artes marciales, movimientos que desafían la gravedad, héroes personalizables dotados de habilidades épicas y un enorme arsenal de armas',45,'NVIDIA GeForce GTX 750TI','Intel i5 4th generation','8 GB de RAM','NVIDIA GeForce GTX 1060 6G','Intel i7 7th generation','16 GB de RAM','https://youtu.be/dQw4w9WgXcQ',1,1,8),(23,'Back 4 Blood',3999,'Los creadores de la aclamada saga de Left 4 Dead presentan Back 4 Blood, un emocionante juego de disparos en primera persona cooperativo',NULL,'NVIDIA GeForce GTX 1050 Ti','Intel Core i5-6600 (3.3 GHz)','8 GB de RAM','NVIDIA GeForce GTX 970','Intel Core i5-8400 (2.8 GHz)','12 GB de RAM','https://youtu.be/dQw4w9WgXcQ',1,1,8),(24,'Call of Duty®: Back Ops III',600,'Call of Duty® Black Ops III: Zombies Chronicles Edition incluye el juego original completo y la expansión de contenido Zombies Chronicles.',30,'NVIDIA® GeForce® GTX 470 @ 1GB','Intel® Core™ i3-530 @ 2.93 GHz ','6 GB de RAM','NVIDIA® GeForce® GTX 470 @ 1GB','Intel® Core™ i3-530 @ 2.93 GHz','6 GB de RAM','https://youtu.be/dQw4w9WgXcQ',2,2,54);
+INSERT INTO `products` VALUES (1,'The Witcher 3',600,'Mientras la guerra se extiende por los Reinos del Norte, aceptarás el contrato de tu vida: encontrar a la niña de la profecía, un arma viviente que puede alterar el mundo tal y como lo conocemos. ',15,'1050ti','I5 7400','8gb','1070ti','i7 8100','16gb','https://www.youtube.com/embed/c0i88t0Kacs?start=15',11,12,7),(2,'Battlefield 4',5500,'Esta es la mejor experiencia de Battlefield 4. Vive el mayor conflicto de la humanidad con un completo arsenal de armas, vehículos y dispositivos, y todos los contenidos de personalización de los dos primeros años ',25,'Nvidia GeForce 8800 GT','(Intel): Core 2 Duo 2.4 GHz','4 GB','NVIDIA GeForce GTX 660','(Intel): Quad-core CPU','8 GB de RAM','https://www.youtube.com/embed/ASzOzrB-a9E?start=2',2,2,8),(11,'Ni no Kuni™ II: Revenant Kingdom',649,'Únete al viaje que llevará al joven rey Evan en busca de un nuevo reino, además de, con la ayuda de nuevos amigos, unir el mundo y salvar a sus gentes de un mal espantoso. ',0,'NVIDIA GeForce GTX 750 Ti','Intel Core i5-4460','4 GB de RAM','NVIDIA GeForce GTX 970','Intel Core i7-3770','8 GB de RAM','https://www.youtube.com/embed/2MJs869kavY?start=4',1,1,7),(12,'Hearts of Iron IV',439,'¡Tenemos la victoria al alcance de la mano! Tu capacidad para liderar tu nación es tu arma principal. En el juego de estrategia Hearts of Iron IV podrás ponerte el mando de cualquier nación de la II Guerra Mundial, el conflicto más fascinante de la historia mundial. ',0,'ATI Radeon HD 5850','Intel Core 2 Quad Q9400 @ 2.66 GHz','4 GB de RAM','ATI Radeon HD 6950','Intel Core i5 750 @ 2.66 GHz','4 GB de RAM','https://www.youtube.com/embed/p26A0H3IoO4?start=0',12,16,3),(13,'Europa Universalis IV',439,'Cuatro siglos de historia cobran vida en el juego de gran estrategia de Paradox. Dirige una nación desde el Renacimiento hasta la Revolución en una compleja simulación del comienzo del mundo moderno. Domina el arte de la guerra, la diplomacia y el comercio para cambiar la historia de tu nación.',0,'Nvidia® GeForce™ GTX 460','Intel® Core™ i3-2105','1 GB RAM','Nvidia® GeForce™ GTX 560 Ti','Intel® Core™ i3 3240','8 GB RAM','https://www.youtube.com/embed/hWQ8AXwHPQg?start=0',12,15,2),(15,'The Elder Scrolls IV: Oblivion',240,'The Elder Scrolls IV: Oblivion® Edición Juego del Año presenta uno de los mejores juegos de rol de todos los tiempos como nunca se había visto hasta ahora. Adéntrate en el mundo más detallado y emocionante jamás creado. ',70,'Tarjeta gráfica con 128 MB de VRAM','Intel Pentium 4 a 2 GHz','512 MB de RAM','Tarjeta gráfica con 128 MB de VRAM','Intel Pentium 4 a 2 GHz','512 MB de RAM','https://www.youtube.com/embed/qJnnPh44Rlo?start=0',11,11,1),(17,'Happy Game',272,'Un niño se duerme y queda sumido en una pesadilla horrible. ¿Conseguirás devolverle la felicidad?',0,'DirectX 11 compatible GPU','2 GHz Intel i5','4 GB de RAM','DirectX 11 compatible GPU','2 GHz Intel i5','4 GB de RAM','https://www.youtube.com/embed/6zRXi3nhUaU?start=0',1,1,4),(18,'The Forest',89,'As the lone survivor of a passenger jet crash, you find yourself in a mysterious forest battling to stay alive against a society of cannibalistic mutants. Build, explore, survive in this terrifying first person survival horror simulator',60,'NVIDIA GeForce 8800GT','Intel Dual-Core 2.4 GHz','4 GB de RAM','NVIDIA GeForce GTX 560','Quad Core Processor','4 GB de RAM','https://www.youtube.com/embed/7mwn5U2PNvk?start=15',2,7,9),(19,'Age of Empires IV',2999,'Uno de los juegos de estrategia en tiempo real más queridos vuelve a demostrar todo su esplendor con Age of Empires IV, donde serás la estrella de las épicas batallas históricas que moldearon el mundo que hoy conocemos',0,'AMD Radeon RX Vega 11','Intel Core i5-6300U','8 GB de RAM','Nvidia GeForce 970 GPU','3.6 GHz 6-core (Intel i5)','16 GB de RAM','https://www.youtube.com/embed/Ovt7d34_hRA?start=0',13,18,2),(20,'Dead By Daylight',334,'Dead by Daylight es un juego de horror de multijugador (4 contra 1) en el que un jugador representa el rol del asesino despiadado y los 4 restantes juegan como supervivientes que intentan escapar de él para evitar ser capturados y asesinados',0,'GeForce GTX 460 1GB ','Intel Core i3-4170','8 GB de RAM','AMD HD 8800','AMD FX-8300','16 GB de RAM','https://www.youtube.com/embed/JGhIXLO3ul8?start=5',2,2,3),(22,'Naraka: Bladepoint',1499,'NARAKA: BLADEPOINT es una experiencia mítica de combate y acción para hasta 60 jugadores con enfrentamientos cuerpo a cuerpo basados en las artes marciales, movimientos que desafían la gravedad, héroes personalizables dotados de habilidades épicas y un enorme arsenal de armas',45,'NVIDIA GeForce GTX 750TI','Intel i5 4th generation','8 GB de RAM','NVIDIA GeForce GTX 1060 6G','Intel i7 7th generation','16 GB de RAM','https://www.youtube.com/embed/Q_uBBXiqL1s?start=0',2,7,9),(23,'Back 4 Blood',3999,'Los creadores de la aclamada saga de Left 4 Dead presentan Back 4 Blood, un emocionante juego de disparos en primera persona cooperativo',0,'NVIDIA GeForce GTX 1050 Ti','Intel Core i5-6600 (3.3 GHz)','8 GB de RAM','NVIDIA GeForce GTX 970','Intel Core i5-8400 (2.8 GHz)','12 GB de RAM','https://www.youtube.com/embed/tKlFJbyUsoo?start=4',2,3,9),(24,'Call of Duty®: Back Ops III',600,'Call of Duty® Black Ops III: Zombies Chronicles Edition incluye el juego original completo y la expansión de contenido Zombies Chronicles.',30,'NVIDIA® GeForce® GTX 470 @ 1GB','Intel® Core™ i3-530 @ 2.93 GHz ','6 GB de RAM','NVIDIA® GeForce® GTX 470 @ 1GB','Intel® Core™ i3-530 @ 2.93 GHz','6 GB de RAM','https://www.youtube.com/embed/qAUYNIDEJ6o?start=4',2,2,57);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -154,7 +154,7 @@ CREATE TABLE `products_images` (
   PRIMARY KEY (`id`),
   KEY `productId` (`productId`),
   CONSTRAINT `products_images_ibfk_1` FOREIGN KEY (`productId`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=124 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=125 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -175,13 +175,13 @@ DROP TABLE IF EXISTS `subcategories`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `subcategories` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   `categoryId` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `categoryId` (`categoryId`),
   CONSTRAINT `subcategories_ibfk_1` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -190,7 +190,7 @@ CREATE TABLE `subcategories` (
 
 LOCK TABLES `subcategories` WRITE;
 /*!40000 ALTER TABLE `subcategories` DISABLE KEYS */;
-INSERT INTO `subcategories` VALUES (1,'Rol de aventuras',1),(2,'First Person Shooter',2),(3,'Third Person Shooter',2);
+INSERT INTO `subcategories` VALUES (1,'Rol de aventuras',1),(2,'First Person Shooter',2),(3,'Third Person Shooter',2),(6,'Arcade',2),(7,'Lucha',2),(8,'Peleas callejeras',2),(9,'Buena trama',1),(10,'Puzles',1),(11,'En grupo',11),(12,'Estrategia',11),(13,'Rol de Acción',11),(14,'Rol japonés',11),(15,'Agricultura',12),(16,'Espacio',12),(17,'Cartas y mesa',13),(18,'Defensa de torres',13),(19,'Militares',13),(20,'Carreras',14),(21,'Pesca y caza',14),(22,'',11);
 /*!40000 ALTER TABLE `subcategories` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -210,7 +210,7 @@ CREATE TABLE `users` (
   `rol` int(2) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -219,7 +219,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (10,'pepito','a@b.com','$2a$10$PVUuxr681p.JlMfS2G9ubOcEMbRkmhSDuzKbVE1Vku3e1oZXcDRge','user-1635451000214.jpg',1),(12,'asd','1@1.com','$2a$10$KA5xgO0c7SF0HJXTSdcfW.OlK3SoXVV.AfiAIBAJvWP5jOzJC5r5O','default_user.jpg',0),(16,'asd','1@2.com','$2a$10$R/5j56gxMY6WREnxP6mwp.5GzCIwKV8wvpOVUo.mMRZOinaH1VBVq','default_user.jpg',0);
+INSERT INTO `users` VALUES (10,'pepito','a@b.com','$2a$10$PVUuxr681p.JlMfS2G9ubOcEMbRkmhSDuzKbVE1Vku3e1oZXcDRge','user-1635451000214.jpg',1),(12,'asd','1@1.com','$2a$10$KA5xgO0c7SF0HJXTSdcfW.OlK3SoXVV.AfiAIBAJvWP5jOzJC5r5O','default_user.jpg',0),(16,'asd','1@2.com','$2a$10$R/5j56gxMY6WREnxP6mwp.5GzCIwKV8wvpOVUo.mMRZOinaH1VBVq','default_user.jpg',0),(17,'aver','aver@mail.com','$2a$10$iyu17EX5NxEAR8Ggr5bNae8ZKpvzF7c9zpFQqeZbq6j68FYnxmrLi','default_user.jpg',0);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -236,4 +236,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-11-25 22:08:59
+-- Dump completed on 2021-11-29 19:40:45
