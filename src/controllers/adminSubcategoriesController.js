2d566f9ed21350036637e7bf5fa59c650792d060
EveNavarro <evenavarro.dn@gmail.com>
2021-11-29 18:56:02 -0300

    crud de subcategories terminado

--- src/controllers/adminSubcategoriesController.js
+++ src/controllers/adminSubcategoriesController.js
@@ -11,9 +11,12 @@ module.exports = {
         });
     },
     subcategoryCreateForm: (req, res) => {
-        res.render('admin/subcategoryForm', {
-            userInSession: req.session.userLogged ? req.session.userLogged : ''
-        });
+        db.Category.findAll().then((category) => {
+            res.render('admin/subcategoryForm', {
+                category,
+                userInSession: req.session.userLogged ? req.session.userLogged : ''
+            });
+        })
     },
     subcategoryStore: (req, res) => {
         let errors = validationResult(req);
@@ -26,11 +29,95 @@ module.exports = {
                 res.redirect('/admin/subcategories');
             }).catch(err => console.log(err))
         } else {
-            res.render('admin/subcategoryForm', {
-                errors: errors.mapped(),
-                old: req.body,
-                userInSession: req.session.userLogged ? req.session.userLogged : ''
-            });
+            db.Category.findAll().then((category) => {
+                res.render('admin/subcategoryForm', {
+                    category,
+                    errors: errors.mapped(),
+                    old: req.body,
+                    userInSession: req.session.userLogged ? req.session.userLogged : ''
+                });
+            })
         }
+    },
+    subcategoryEdit: (req, res) => {
+        db.Category.findAll().then((category) => {
+            db.Subcategory.findByPk(req.params.id).then((subcategory) => {
+                res.render('admin/subCategoryEditForm', {
+                    subcategory,
+                    category,
+                    userInSession: req.session.userLogged ? req.session.userLogged : ''
+                })
+            })
+        })
+    },
+    subcategoryUpdate: (req, res) => {
+        let errors = validationResult(req);
+
+        if (errors.isEmpty()) {
+            db.Subcategory.update(
+                {
+                    name: req.body.name,
+                    categoryId: req.body.categoryId
+                },
+                {
+                    where: {
+                        id: req.params.id
+                    }
+                }
+            ).then((result) => {
+                res.redirect('/admin/subcategories')
+            })
+        } else {
+            db.Category.findAll().then((category) => {
+                db.Subcategory.findByPk(req.params.id).then((subcategory) => {
+                    res.render('admin/subCategoryEditForm', {
+                        category,
+                        subcategory,
+                        errors: errors.mapped(),
+                        old: req.body,
+                        userInSession: req.session.userLogged ? req.session.userLogged : ''
+                    })
+                })
+            })
+        }
+    },
+    subcategoryDestroy: (req, res) => {
+        db.Product.findAll({
+            where: {
+                subCategoryId: req.params.id
+            }
+        }).then(products => {
+            products.forEach(product => {
+                db.ProductImage.findAll({
+                    where: {
+                        productId: product.id
+                    }
+                }).then(images => {
+                    images.forEach((image) => {
+                        fs.existsSync("./public/img/games/", image.image)
+                        ? fs.unlinkSync("./public/img/games/" + image.image)
+                        : console.log("--No se encontró");
+                    });
+                })
+                db.ProductImage.destroy({
+                    where: {
+                        productId: product.id
+                    }
+                }).then((result) => {})
+            })
+            db.Product.destroy({
+                where: {
+                    subCategoryId: req.params.id
+                }
+            }).then((result) => {
+                db.Subcategory.destroy({
+                    where: {
+                        id: req.params.id
+                    }
+                }).then((result) => {
+                    return res.redirect('/admin/subcategories')
+                });
+            });
+        })
     }
 }
